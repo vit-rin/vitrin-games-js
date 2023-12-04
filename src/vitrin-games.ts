1de718568f50937ff7041d45d18fb09c86511592
A.J <ali.jamalzad@gmail.com>
2023-12-04 21:26:15 +0330

    refactor: added VitGames as main cunstructor

--- src/vitrin-games.ts
+++ src/vitrin-games.ts
@@ -1,3 +1,36 @@
+import { Ads } from "./Ads";
+import { Check } from "./check";
 import { Score } from "./score";
+import { options } from "./types/options";
 
-export { Score };
+declare global {
+    interface Window {
+        VitGames: typeof VitGames;
+    }
+}
+
+class VitGames {
+    private options: options;
+
+    public score;
+
+    public check;
+
+    public ads;
+
+    constructor(options: options) {
+        this.options = options;
+
+        this.score = new Score(this.options);
+
+        this.check = new Check(this.options);
+
+        this.ads = new Ads(this.options);
+    }
+}
+
+export { VitGames };
+
+if (typeof window !== "undefined") {
+    window.VitGames = VitGames;
+}
