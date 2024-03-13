79f5ff028427968d4518b1495e6406e4840d124f
A.J <ali.jamalzad@gmail.com>
2024-03-13 22:31:12 +0330

    feat: added muteCallback, unmuteCallback, pauseCallback and resumeCallback callbacks to options

--- src/types/options.ts
+++ src/types/options.ts
@@ -1,3 +1,7 @@
 export type options = {
     gameId: string | undefined;
+    muteCallback: Function | undefined;
+    unmuteCallback: Function | undefined;
+    pauseCallback: Function | undefined;
+    resumeCallback: Function | undefined;
 };
