314a589030fd625e2e7a04eb6beeceb5c36368a9
A.J <ali.jamalzad@gmail.com>
2024-03-30 17:50:04 +0330

    fix: end screen pixel perfect design and added loading for result

--- src/components/EndScreen.tsx
+++ src/components/EndScreen.tsx
@@ -12,6 +12,7 @@ import { competitionResultState } from "../states/competitionResultState";
 import { gameDataState } from "../states/gameDataState";
 import VTonIcon from "./VTonIcon";
 import XPIcon from "./XPIcon";
+import LoadingSpinner from "./LoadingSpinner";
 
 export default function EndScreen() {
     const options: OptionsType = Options.getInstance().get();
@@ -34,99 +35,108 @@ export default function EndScreen() {
     return (
         <>
             {gameCurrent === "ended" && competitionResult && (
-                <div className="tw-flex tw-justify-center tw-items-center tw-fixed tw-h-screen tw-w-full tw-top-0 tw-left-0 tw-right-0 tw-bottom-0 tw-bg-black tw-bg-opacity-80 tw-z-50">
+                <div className="tw-flex tw-justify-center tw-items-center tw-fixed tw-h-screen tw-w-full tw-top-0 tw-left-0 tw-right-0 tw-bottom-0 tw-bg-[#1F202399] tw-z-50">
                     <div className="tw-container">
-                        {competitionResult.result === "win" && (
-                            <div className="tw-mb-4">
-                                <WinIcon className="tw-mx-auto" />
-                            </div>
-                        )}
-
-                        {competitionResult.result !== "unknown" && (
-                            <>
-                                <div
-                                    className={`tw-text-5xl tw-text-center tw-font-[capsule] tw-mb-4 tw-capitalize ${
-                                        competitionResult.result === "win"
-                                            ? "tw-text-[#FFBF44]"
-                                            : "tw-text-[#A6A8AB]"
-                                    }`}
-                                >
-                                    You{" "}
-                                    {competitionResult.result === "loss"
-                                        ? "lose"
-                                        : competitionResult.result}
-                                    {competitionResult.result === "win" && "!"}
+                        <div className="tw-bg-[#404041] tw-rounded-[2rem] tw-pt-16 tw-pb-4 tw-px-4">
+                            {competitionResult.result === "win" && (
+                                <div className="tw-mb-4">
+                                    <WinIcon className="tw-mx-auto" />
                                 </div>
+                            )}
 
-                                <div className="tw-text-2xl tw-text-white tw-text-center tw-font-[capsule] tw-mb-8">
-                                    <span
-                                        className={`${
-                                            competitionResult.result === "win"
-                                                ? "tw-text-[#FFBF44]"
-                                                : "tw-text-[#A6A8AB]"
-                                        }`}
-                                    ></span>
-                                    <span
-                                        className={`${
+                            {competitionResult.result !== "unknown" ? (
+                                <>
+                                    <div
+                                        className={`tw-text-5xl tw-text-center tw-font-[capsule] tw-mb-4 tw-capitalize ${
                                             competitionResult.result === "win"
                                                 ? "tw-text-[#FFBF44]"
                                                 : "tw-text-[#A6A8AB]"
                                         }`}
                                     >
-                                        {score}{" "}
-                                        {competitionResult.result === "win"
-                                            ? ">"
-                                            : competitionResult.result === "tie"
-                                            ? "="
-                                            : "<"}
-                                    </span>{" "}
-                                    {gameData.metadata.min_score_to_reward}
+                                        You{" "}
+                                        {competitionResult.result === "loss"
+                                            ? "lose"
+                                            : competitionResult.result}
+                                        {competitionResult.result === "win" &&
+                                            "!"}
+                                    </div>
+
+                                    <div className="tw-text-2xl tw-text-white tw-text-center tw-font-[capsule] tw-mb-8">
+                                        <span
+                                            className={`${
+                                                competitionResult.result ===
+                                                "win"
+                                                    ? "tw-text-[#FFBF44]"
+                                                    : "tw-text-[#A6A8AB]"
+                                            }`}
+                                        ></span>
+                                        <span
+                                            className={`${
+                                                competitionResult.result ===
+                                                "win"
+                                                    ? "tw-text-[#FFBF44]"
+                                                    : "tw-text-[#A6A8AB]"
+                                            }`}
+                                        >
+                                            {score}{" "}
+                                            {competitionResult.result === "win"
+                                                ? ">"
+                                                : competitionResult.result ===
+                                                  "tie"
+                                                ? "="
+                                                : "<"}
+                                        </span>{" "}
+                                        {gameData.metadata.min_score_to_reward}
+                                    </div>
+
+                                    {competitionResult.transaction && (
+                                        <div className="tw-bg-[#1F2023] tw-text-[#E0E2E2] tw-rounded-2xl tw-p-4 tw-mb-4 tw-flex tw-justify-start tw-items-center tw-font-[capsule] tw-w-full">
+                                            {competitionResult.transaction.data
+                                                .wallet.data.slug ===
+                                                "v-ton" && <VTonIcon />}
+
+                                            <span className="tw-ml-4 tw-text-2xl">
+                                                +
+                                                {
+                                                    competitionResult
+                                                        .transaction.data.amount
+                                                }
+                                            </span>
+                                        </div>
+                                    )}
+
+                                    {competitionResult.rewards && (
+                                        <div className="tw-bg-[#1F2023] tw-text-[#E0E2E2] tw-rounded-2xl tw-p-4 tw-mb-4 tw-flex tw-justify-start tw-items-center tw-font-[capsule] tw-w-full">
+                                            <XPIcon />
+
+                                            <span className="tw-ml-4 tw-text-2xl">
+                                                +{competitionResult.rewards.xp}
+                                            </span>
+                                        </div>
+                                    )}
+                                </>
+                            ) : (
+                                <div className="tw-flex tw-justify-center tw-mb-8">
+                                    <LoadingSpinner />
                                 </div>
+                            )}
 
-                                {competitionResult.transaction && (
-                                    <div className="tw-bg-[#1F2023] tw-text-[#E0E2E2] tw-rounded-2xl tw-p-4 tw-mb-4 tw-flex tw-justify-start tw-items-center tw-font-[capsule] tw-w-full">
-                                        {competitionResult.transaction.data
-                                            .wallet.data.slug === "v-ton" && (
-                                            <VTonIcon />
-                                        )}
+                            <button
+                                className="tw-bg-[#8B6BAF] tw-font-[capsule] tw-font-bold tw-text-white tw-text-lg tw-p-4.25 tw-w-full tw-h-14 tw-rounded-xl tw-flex tw-justify-center tw-items-center tw-mb-4"
+                                onClick={replay}
+                            >
+                                <ReplayIcon />
+                                <span className="tw-ml-2">Replay</span>
+                            </button>
 
-                                        <span className="tw-ml-4 tw-text-2xl">
-                                            +
-                                            {
-                                                competitionResult.transaction
-                                                    .data.amount
-                                            }
-                                        </span>
-                                    </div>
-                                )}
-
-                                {competitionResult.rewards && (
-                                    <div className="tw-bg-[#1F2023] tw-text-[#E0E2E2] tw-rounded-2xl tw-p-4 tw-mb-4 tw-flex tw-justify-start tw-items-center tw-font-[capsule] tw-w-full">
-                                        <XPIcon />
-
-                                        <span className="tw-ml-4 tw-text-2xl">
-                                            +{competitionResult.rewards.xp}
-                                        </span>
-                                    </div>
-                                )}
-                            </>
-                        )}
-
-                        <button
-                            className="tw-bg-[#8B6BAF] tw-font-[capsule] tw-font-bold tw-text-white tw-text-lg tw-p-4.25 tw-w-full tw-h-14 tw-rounded-xl tw-flex tw-justify-center tw-items-center tw-mb-4"
-                            onClick={replay}
-                        >
-                            <ReplayIcon />
-                            <span className="tw-ml-2">Replay</span>
-                        </button>
-
-                        <button
-                            className="tw-border-2 tw-border-white tw-font-[capsule] tw-font-bold tw-text-white tw-text-lg tw-p-4.25 tw-w-full tw-h-14 tw-rounded-xl tw-flex tw-justify-center tw-items-center"
-                            onClick={exit}
-                        >
-                            <ExitIcon />
-                            <span className="tw-ml-2">Exit</span>
-                        </button>
+                            <button
+                                className="tw-border-2 tw-border-white tw-font-[capsule] tw-font-bold tw-text-white tw-text-lg tw-p-4.25 tw-w-full tw-h-14 tw-rounded-xl tw-flex tw-justify-center tw-items-center"
+                                onClick={exit}
+                            >
+                                <ExitIcon />
+                                <span className="tw-ml-2">Exit</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
             )}
