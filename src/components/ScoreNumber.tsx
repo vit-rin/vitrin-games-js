cbc8ba71e270213aecf709b247d94b358f30899c
A.J <ali.jamalzad@gmail.com>
2024-04-10 21:54:46 +0330

    fix: set score number color based on competition target score

--- src/components/ScoreNumber.tsx
+++ src/components/ScoreNumber.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { scoreState } from "../states/scoreState";
 import { gameDataState } from "../states/gameDataState";
@@ -13,22 +13,21 @@ export default function ScoreNumber() {
     const [gameData] = useRecoilState<any>(gameDataState);
     const [gameEnded] = useRecoilState<any>(gameEndedState);
     const [competitionData] = useRecoilState<any>(competitionDataState);
+    const [scoreNumberColor, setScoreNumberColor] = useState("");
+
+    useEffect(() => {
+        if (competitionData) {
+            if (score > competitionData.target_score) {
+                setScoreNumberColor("tw-text-[#FFBF44]");
+            }
+        }
+    }, [score]);
 
     return (
         <div className="tw-font-bold tw-font-[capsule] tw-text-white tw-text-xl">
             {gameData && (
                 <>
-                    <span
-                        className={`${
-                            (competition.getType() == "solo" &&
-                                score >
-                                    gameData.metadata.min_score_to_reward) ||
-                            (competition.getType() == "pvp" &&
-                                competitionData &&
-                                score > competitionData.target_score &&
-                                "tw-text-[#FFBF44]")
-                        }`}
-                    >
+                    <span className={scoreNumberColor}>
                         {score} {competition.getType() == "solo" && ">"}
                         {competition.getType() == "pvp" && score == 0 && "-"}
                         {competition.getType() == "pvp" && score > 0 && ">"}
